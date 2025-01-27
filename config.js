10e57e57ec067c28f7fb87ca7bdc03a3de964ce4
Prasannakumara <153364276+Prasannakumara@users.noreply.github.com>
2025-01-27 06:43:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //put your session id
+SESSION_ID: process.env.SESSION_ID || "5swnFLxS#T1Ay5xMj4OxxFdnU1VtxrPTvCgCe23YGGJ4AzLDHPoM", //put your session id
 MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
 MODE: process.env.MODE || "private", //private | public | groups | inbox
 STATUES_REPLY: process.env.STATUES_REPLY || "false", //true or false
